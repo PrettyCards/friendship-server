ee11a51d45e220fadb37fe8b5d82961ca9839c73
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-03-01 11:48:27 +0200

    Quick Test with Copied Code from feildmaster. Will change soon.

---
+++ src/index.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+const needle = require('needle');
+	
+const {
+	access,
+	readFile,
+	writeFile,
+	mkdir,
+} = fs.promises;
+
+const from = 'https://undercards.net/Leaderboard?action=ranked';
+
+const safeReadData = ['id', 'username', 'winsRanked', 'lossesRanked', 'division', 'level', 'eloRanked'];
+
+function loadChanges(type = 'daily', skipCommit = '') {
+	const rankingsPath = `./data/rankings-${type}.json`;
+	return needle(from)
+	// Parse the leaderboard
+	.then((res) => JSON.parse(res.body.leaderboard))
+	// Trim excess data
+	.then((rankings = []) => rankings.map((data) => safeReadData.reduce((ret, key) => (ret[key] = data[key], ret), {})))
+	// Load old rankings
+	.then((rankings = []) => access(rankingsPath, fs.constants.F_OK | fs.constants.W_OK)
+		// Parse old rankings
+		.then(() => readFile(rankingsPath).then(JSON.parse))
+		.catch((e) => {
+		if (e.code !== 'ENOENT') {
+			throw new Error('Not writable'); // Shouldn't ever happen, really.
+		}
+		})
+		// Convert to lookup-by-id
+		.then((old = []) => old.reduce((ret, val, i) => (val.position = i, ret[val.id] = val, ret), {}))
+		.then((old = {}) => {
+		rankings.forEach((data, position) => {
+			// Calculate rank change and elo change
+			const prev = old[data.id];
+			if (prev) {
+			data.eloChange = data.eloRanked - prev.eloRanked; // Higher elo is better
+			data.rankChange = prev.position - position; // Lower position is better
+			data.wins = data.winsRanked - prev.winsRanked;
+			data.losses = data.lossesRanked - prev.lossesRanked;
+			}
+			data.rank = position + 1;
+			// data.bestRank = Math.min(position, prev ? prev.bestRank : Infinity);
+			// data.eloBest = Math.max(data.eloRanked, prev ? prev.eloBest : 0);
+		});
+		return rankings;
+		})
+		.then((rankings = []) => skipCommit ? rankings : Promise.all(rankings.map((data) => writeFile(`./data/users/${data.id}.json`, JSON.stringify(data, undefined, 2))))
+			.then(() => rankings))
+		.then((rankings = []) => skipCommit ? rankings : writeFile(rankingsPath, JSON.stringify(rankings, undefined, 2))
+		.then(() => rankings))
+		.then((rankings = []) => {
+		const columns = [{
+			key: 'rank',
+			format: (text = '', { rankChange }) => change(text, rankChange),
+		}, {
+			key: 'level',
+			name: 'LV',
+		}, {
+			key: 'username',
+			name: 'Player',
+			format: (text = '', { id }) => `<span title="ID: ${id}">${text}</span>`,
+		}, {
+			key: 'division',
+			format: (text = '') => text.replace('_', ' '),
+		}, {
+			key: 'eloRanked',
+			name: 'Elo',
+			format: (text = '', { eloChange }) => change(text, eloChange),
+		}, {
+			key: 'winsRanked',
+			name: 'Wins',
+			format: (text = '', { wins }) => change(text, wins),
+		}, {
+			key: 'lossesRanked',
+			name: 'Losses',
+			format: (text = '', { losses }) => change(text, losses),
+		}];
+
+		const top5 = [
+			`---`,
+			`title: ${capitalize(type)} Update`,
+			'layout: leaderboard',
+			`category: ${type}`,
+			`---`,
+			'',
+			'{: .leaderboard}',
+			`| ${columns.map(({ name = '', key = '' }) => name || capitalize(key)).join(' | ')} |`,
+			`| ${columns.map(() => '---').join(' | ')} |`,
+			`${rankings.slice(0, 5).map((entry) => `| ${columns.map(({ 
+			key = '',
+			format = (text, entry) => text,
+			}) => format(entry[key], entry)).join(' | ')} |`).join('\n')}`,
+		].join('\n');
+
+		// Save post
+		const slug = Math.floor(Date.now() / 1000);
+		const postData = `./docs/assets/data/${type}`;
+		const postPath = `./docs/_${type}`;
+		return mkdir(postData, {
+			recursive: true,
+		}).then(() => writeFile(`${postData}/${slug}.json`, JSON.stringify(rankings))).then(() => mkdir(postPath, {
+			recursive: true,
+		}).then(() => writeFile(`${postPath}/${slug}.md`, top5)));
+		}));
+}
+
+function capitalize(text = '') {
+	return text[0].toUpperCase() + text.substring(1);
+}
+
+function change(text, data) {
+	return `<span data-change="${data === undefined ? '-' : data}">${text}</span>`;
+}
+
+loadChanges(...process.argv.slice(2)).catch((e) => {
+	console.error(e);
+	process.exit(1);
+});
